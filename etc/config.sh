d322563b538a48e34ce0a1d2ca72bcad033ec897
Andreas Schuh <andreas.schuh.84@gmail.com>
2014-05-18 22:57:58 +0100

    Add kernel files for sigma=1 generated by the "kernel" script for age range 28-44 weeks PMA.

--- etc/config.sh
+++ etc/config.sh
@@ -3,22 +3,22 @@ PATH="/vol/biomedic/users/as12312/local/linux-3.6/irtk-nnatlas:$PATH"
 LD_LIBRARY_PATH='/vol/biomedic/users/as12312/local/lib'
 
 # input settings
-topdir="$appdir"         # top-level/working directory
-imgdir='../images'       # anatomical brain images
-lbldir='../labels'       # input tissue and structure segmentations
-bgvalue=0                # background value of skull-stripped images
+topdir="$appdir"                 # top-level/working directory
+imgdir='../images'               # anatomical brain images
+lbldir='../labels'               # input tissue and structure segmentations
+bgvalue=0                        # background value of skull-stripped images
 
 # kernel regression
-epsilon=0.001            # kernel weight threshold
-sigma=1                  # (default) standard deviation of Gaussian
-kernel='etc/kernel'      # directory containing temporal kernel files
+epsilon=0.001                    # kernel weight threshold
+sigma=1                          # (default) standard deviation of Gaussian
+kernel="etc/kernel_sigma=$sigma" # directory containing temporal kernel files
 
 # output settings
-bindir='bin'             # job executable files
-libdir='lib'             # dependencies of job executables
-dagdir='dag'             # DAG files for HTCondor DAGMan job
-logdir='log'             # log files written by HTCondor jobs
-dofdir='../dofs'         # transformations computed during atlas construction
+bindir='bin'                     # job executable files
+libdir='lib'                     # dependencies of job executables
+dagdir='dag'                     # DAG files for HTCondor DAGMan job
+logdir='log'                     # log files written by HTCondor jobs
+dofdir='../dofs'                 # transformations computed during atlas construction
 
 # HTCondor settings
 notify_user='as12312@imperial.ac.uk'
