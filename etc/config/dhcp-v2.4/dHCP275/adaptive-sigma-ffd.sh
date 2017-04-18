d2728ac478427aa00f9d3a3839a1c36c387e9432
Andreas Schuh <andreas.schuh.84@gmail.com>
2017-04-18 03:58:13 +0100

    add: dHCP275 atlas construction config using classic FFD

---
+++ etc/config/dhcp-v2.4/dHCP275/adaptive-sigma-ffd.sh
@@ -0,0 +1,32 @@
+## See etc/config/default.sh for documentation and full list of parameters
+##
+## This atlas construction is based on the original spatio-temporal neonatal
+## atlas construction approach proposed by Serag et al.
+
+set_pardir_from_file_path "$BASH_SOURCE"
+source "$topdir/$pardir/adaptive-sigma.sh"
+
+# registration
+mffd='Sum'
+model='Affine+FFD'
+levels=4
+resolution=0.5
+interpolation='Linear with padding'
+similarity='NMI'
+bins=64
+inclbg=false
+bending=1e-3
+jacobian=0
+symmetric=false
+pairwise=true
+refine=0
+
+# output settings
+subdir="dHCP275/adaptive-sigma_$sigma-ffd"
+dagdir="dag/$subdir"
+logdir="log/$subdir"
+dofdir="../$subdir/dofs"
+evldir="../$subdir/eval"
+outdir="../$subdir/atlas"
+tmpdir="../$subdir/temp"
+log="$logdir/progress.log"
