bdc35ce8ac1dd2ce1190efe7e1238df3cec74c64
Andreas Schuh <andreas.schuh.84@gmail.com>
2017-05-04 19:55:18 +0100

    add: adaptive-sigma-norefine/constant-sigma-norefine config files

---
+++ etc/config/dhcp-v2.4/dHCP275/constant-sigma-norefine.sh
@@ -0,0 +1,15 @@
+## See etc/config/default.sh for documentation and full list of parameters
+
+set_pardir_from_file_path "$BASH_SOURCE"
+source "$topdir/$pardir/constant-sigma.sh"
+
+refine=0
+
+subdir="dHCP275/constant-sigma_$sigma-norefine"
+dagdir="dag/$subdir"
+logdir="log/$subdir"
+dofdir="../$subdir/dofs"
+evldir="../$subdir/eval"
+outdir="../$subdir/atlas"
+tmpdir="../$subdir/temp"
+log="$logdir/progress.log"
