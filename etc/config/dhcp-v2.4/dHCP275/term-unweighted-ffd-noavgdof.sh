85bd11738e2ffc296d9fc5415968062c0a9fae16
Andreas Schuh <andreas.schuh.84@gmail.com>
2017-05-05 22:41:37 +0100

    add: Config for unweighted term atlas construction without pairwise registrations

---
+++ etc/config/dhcp-v2.4/dHCP275/term-unweighted-ffd-noavgdof.sh
@@ -0,0 +1,17 @@
+## See etc/config/default.sh for documentation and full list of parameters
+
+set_pardir_from_file_path "$BASH_SOURCE"
+source "$topdir/$pardir/term-unweighted-ffd-noresdof.sh"
+
+# registration
+pairwise=false
+
+# output settings
+subdir="dHCP275/term-unweighted-ffd-noavgdof"
+dagdir="dag/$subdir"
+logdir="log/$subdir"
+dofdir="../$subdir/dofs"
+evldir="../$subdir/eval"
+outdir="../$subdir/atlas"
+tmpdir="../$subdir/temp"
+log="$logdir/progress.log"
