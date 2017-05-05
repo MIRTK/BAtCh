8137f63331dcaedd3cd2c327dcd74d3221db5d16
Andreas Schuh <andreas.schuh.84@gmail.com>
2017-05-05 22:42:24 +0100

    fix: Change FFD-based term atlas construction config

--- etc/config/dhcp-v2.4/dHCP275/term-unweighted-ffd-noresdof.sh
+++ etc/config/dhcp-v2.4/dHCP275/term-unweighted-ffd-noresdof.sh
@@ -1,7 +1,4 @@
 ## See etc/config/default.sh for documentation and full list of parameters
-##
-## This atlas construction is based on the original spatio-temporal neonatal
-## atlas construction approach proposed by Serag et al., NeuroImage 2012
 
 set_pardir_from_file_path "$BASH_SOURCE"
 source "$topdir/$pardir/term-unweighted.sh"
@@ -15,13 +12,12 @@ interpolation='Linear with padding'
 similarity='NMI'
 bins=64
 inclbg=false
-spacing=2.0
+spacing=2.5
 bending=1e-3
 jacobian=0
 symmetric=false
 pairwise=true
 useresdof=false
-refine=0
 
 # output settings
 subdir="dHCP275/term-unweighted-ffd-noresdof"
