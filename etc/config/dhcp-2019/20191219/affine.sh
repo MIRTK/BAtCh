f21972b6c3e8257d479f011d604f1920f5a9b780
Andreas Schuh <andreas.schuh.84@gmail.com>
2019-12-19 20:54:49 +0000

    Add scripts and configs used for new dHCP atlas construction (global normalization)

---
+++ etc/config/dhcp-2019/20191219/affine.sh
@@ -0,0 +1,22 @@
+## See etc/config/default.sh for documentation and full list of parameters
+##
+## Construct affine spatio-temporal atlas; no deformable registrations.
+
+set_pardir_from_file_path "$BASH_SOURCE"
+source "$topdir/$pardir/common.sh"
+
+# registration parameters
+pairwise=false
+refine=0
+
+# output settings
+subsubdir="affine"
+dagdir="dag/$subdir/$subsubdir"
+logdir="log/$subdir/$subsubdir"
+log="$logdir/progress.log"
+
+resdir="out/$subdir/$subsubdir"
+dofdir="../$resdir/dofs"
+evldir="../$resdir/eval"
+outdir="../$resdir/atlas"
+tmpdir="../$resdir/temp"
---
+++ etc/config/dhcp-2019/20191219/common.sh
@@ -0,0 +1,64 @@
+## See etc/config/default.sh for documentation and full list of parameters
+
+set_pardir_from_file_path "$BASH_SOURCE"
+
+# input
+agelst="$pardir/ages.csv"
+sublst="$pardir/subjects.lst"
+
+input_dir="/vol/dhcp-derived-data/volumetric-atlases/groupwise-v2/input"
+
+t1wdir="$input_dir/images/t1w"
+t1wpre=""
+t1wsuf=".nii.gz"
+
+t2wdir="$input_dir/images/t2w"
+t2wpre=""
+t2wsuf=".nii.gz"
+
+imgdir="$t2wdir"
+imgpre="$t2wpre"
+imgsuf="$t2wsuf"
+
+bgvalue=0
+
+lbldir="$input_dir/labels/structures"
+lblpre=""
+lblsuf=".nii.gz"
+
+clsdir="$input_dir/labels/tissues"
+clspre=""
+clssuf=".nii.gz"
+
+tissues=9
+structures=87
+
+segdir="$input_dir/masks"
+segpre=""
+segsuf=".nii.gz"
+
+# spatial normalization
+refdir="etc/reference"
+refpre=""
+refsuf=".nii.gz"
+refid="schuh-t40"
+refini=true
+
+# regression
+means=({29..44})
+sigma=1.0
+sigma=$(printf '%.2f' $sigma)
+epsilon=0.001
+kernel="$pardir/weights"
+krnpre="t"
+krnext="tsv"
+
+# output settings
+subdir="dhcp-atlas-v2"
+dagdir="dag/$subdir"
+logdir="log/$subdir"
+dofdir="../$subdir/dofs"
+evldir="../$subdir/eval"
+outdir="../$subdir/atlas"
+tmpdir="../$subdir/temp"
+log="$logdir/progress.log"
