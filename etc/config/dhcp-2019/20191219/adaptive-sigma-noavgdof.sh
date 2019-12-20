988c9d3f4d1e3b3749618f50d6308f799b4df4a6
Andreas Schuh <andreas.schuh.84@gmail.com>
2019-12-20 13:59:43 +0000

    enh: Add dHCP 2019 configuration file for iterative template construction

---
+++ etc/config/dhcp-2019/20191219/adaptive-sigma-noavgdof.sh
@@ -0,0 +1,19 @@
+## See etc/config/default.sh for documentation and full list of parameters
+
+set_pardir_from_file_path "$BASH_SOURCE"
+source "$topdir/$pardir/common.sh"
+
+# registration parameters
+pairwise=false
+
+# output settings
+subdir="adaptive-sigma-noavgdof"
+dagdir="dag/$subdir"
+logdir="log/$subdir"
+log="$logdir/progress.log"
+
+resdir="out/$subdir"
+dofdir="../$resdir/dofs"
+evldir="../$resdir/eval"
+outdir="../$resdir/atlas"
+tmpdir="../$resdir/temp"
